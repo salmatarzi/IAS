1ee26b5380fd66f041cb24a1c5e1f4a23fa00fa0
abdelrahman kamel <a.kamel_93@hotmail.com>
2015-03-17 17:11:14 +0200

    #10 adentpoin

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
-
 var usersRoute = require('./routes/users');
 var publishersRoute = require('./routes/publishers');
+var app = express();
+var adRequest = require('./routes/adRequest');
 
+app.use('/adRequest',adRequest);
 app.use('/users', usersRoute);
 app.use('/publishers', publishersRoute);
 
